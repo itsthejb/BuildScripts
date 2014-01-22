fa697c369e361a6d05b8ec1536ec2c60d93f5a4b
Jonathan Crooke <joncrooke@gmail.com>
2014-01-22 14:56:43 +0000

    script for detached head/Jenkins

--- BuildHeader.sh
+++ BuildHeader.sh
@@ -1,10 +1,18 @@
-PREFIX=`echo ${PROJECT_NAME} | awk '{print toupper($0)}'`
+#!/bin/bash
 
+# Script assumes that the origin repo is 'origin'. If not, change here
+ORIGIN="origin"
+
+PREFIX=`echo ${PROJECT_NAME} | awk '{print toupper($0)}'`
 BUILD_FILENAME="${PROJECT_NAME}Build.h"
 BUILD_HEADER="${BUILT_PRODUCTS_DIR}/$BUILD_FILENAME"
 
 GIT_REV_SHA=$(git --git-dir="${PROJECT_DIR}/.git" --work-tree="${PROJECT_DIR}" rev-parse --short HEAD)
 GIT_BRANCH=$(git --git-dir="${PROJECT_DIR}/.git" --work-tree="${PROJECT_DIR}" symbolic-ref --short -q HEAD)
+if [ -z $GIT_BRANCH ]; then
+    # Fallback for detached HEAD state (probably on Jenkins?)
+    GIT_BRANCH=$(git rev-parse HEAD | git branch -a --contains | grep remotes | sed -e "s/.*remotes.$ORIGIN.//")
+fi
 
 rm -f ${BUILD_HEADER} &> /dev/null
 
