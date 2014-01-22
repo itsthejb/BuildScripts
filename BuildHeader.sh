946ffd308cf31222fd422c2693522daabf02f23f
Jonathan Crooke <joncrooke@gmail.com>
2014-01-22 11:26:59 +0000

    Tweaks

--- BuildHeader.sh
+++ BuildHeader.sh
@@ -1,10 +1,12 @@
 PREFIX=`echo ${PROJECT_NAME} | awk '{print toupper($0)}'`
 
-BUILD_FILENAME="${PREFIX}Build.h"
+BUILD_FILENAME="${PROJECT_NAME}Build.h"
 BUILD_HEADER="${BUILT_PRODUCTS_DIR}/$BUILD_FILENAME"
 
 GIT_REV_SHA=$(git --git-dir="${PROJECT_DIR}/.git" --work-tree="${PROJECT_DIR}" rev-parse --short HEAD)
 GIT_BRANCH=$(git --git-dir="${PROJECT_DIR}/.git" --work-tree="${PROJECT_DIR}" symbolic-ref --short -q HEAD)
+
+rm -f ${BUILD_HEADER} &> /dev/null
 
 echo "// $BUILD_FILENAME" > $BUILD_HEADER
 echo "#define ${PREFIX}_BUILD_SHA @\"$GIT_REV_SHA\"" >> $BUILD_HEADER
