aad801c97ba0d26fd321d946977d7419ce26b7f7
Jonathan Crooke <joncrooke@gmail.com>
2014-01-23 11:12:08 +0000

    Remote and local branches

--- BuildHeader.rb
+++ BuildHeader.rb
@@ -19,25 +19,33 @@ path = built_products_dir + '/' + filename
 
 # TODO: use Grit
 git_sha = %x[ #{'git --git-dir="' + project_dir + '/.git" --work-tree="' + project_dir + '" rev-parse --short HEAD'} ].strip
-git_branch = %x[ #{'git --git-dir="' + project_dir + '/.git" --work-tree="' + project_dir + '" symbolic-ref --short -q HEAD'} ].strip
+
+# There are easier ways to do this IF you can guarantee you aren't in a detached HEAD state
+# So this more convoluted method is more flexible
+git_branch = %x[ #{'git show-ref | grep `git rev-parse HEAD` | grep -v HEAD | grep refs/remotes/origin | sed -e "s/.*`git rev-parse HEAD` refs\/remotes\/' +  origin + './/"'} ].split("\n")
 
 if git_branch.length == 0
-  git_branch = %x[ #{'git rev-parse HEAD | git branch -a --contains | grep remotes | sed -e "s/.*remotes.' +  $origin + './/"'} ].strip
+  # Zero length - this commit has not yet been pushed - can use local branch
+  git_branch = %x[ #{'git --git-dir="' + project_dir + '/.git" --work-tree="' + project_dir + '" symbolic-ref --short -q HEAD'} ]
 end
 
+# Strip the guff
+git_branch = Array(git_branch).map { |branch| branch.strip }
+
 # We may have multiple branches, so we always create a set
-git_branch = '[NSSet setWithArray:@[ @"' + Array(git_branch).join('", @"') + '" ]]'
+git_branch = '[NSSet setWithArray:@[ @"' + git_branch.join('", @"') + '" ]]'
 
 if Dir.exists?(path)
   Dir.delete(path)
 end
 
 def buildDefine(name, value, quote)
-  quote = (quote ? '@"' : '')
-  return "\n#define " + $prefix + '_' + name + ' ' + quote + value + quote
+  prefix = (quote ? '@"' : '')
+  suffix = (quote ? '"' : '')
+  return "\n#define " + $prefix + '_' + name + ' ' + prefix + value + suffix
 end
 
-header_text = "//\n// " + filename + "\n//\n" +
+header_text = "//\n// " + filename + "\n//" +
 buildDefine('BUILD_SHA', git_sha, true) +
 buildDefine('BUILD_SHA_BRANCHES', git_branch, false) +
 buildDefine('BUILD_DATE', Date.today.to_s, true) +
